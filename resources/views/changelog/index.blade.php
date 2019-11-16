5b8f409eae01a353103dd7875fc2441c912affee
Meng Xuan Xia <mengxuan.xia@outlook.com>
2019-11-15 22:13:45 -0500

    change logs

--- resources/views/changelog/index.blade.php
+++ resources/views/changelog/index.blade.php
@@ -6,10 +6,6 @@
     <section class="ph3 ph0-ns pv4 cf w-100 bg-gray-monica">
       <div class="mt4 mw7 center mb3">
         <h3>{{ trans('changelog.title') }}</h3>
-
-        @if (\App::getLocale() != 'en')
-        <p>{{ trans('changelog.note') }}</p>
-        @endif
       </div>
 
       @foreach ($changelogs as $changelog)
