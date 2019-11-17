7b4858823c5bd0b0ef2293dd5358856724e1fa94
Meng Xuan Xia <mengxuan.xia@outlook.com>
2019-11-17 17:19:09 -0500

    Revert "sentry debugger"
    This reverts commit fe367685

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
 Route::get('/', 'LandingController@index')->name('index');
 Route::get('/privacy', 'PrivacyController@index')->name('index');
 Route::get('/terms', 'TermsController@index')->name('index');
