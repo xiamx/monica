63b1da601b19823f659583127cb88b3379eaa1fe
Meng Xuan Xia <mengxuan.xia@outlook.com>
2019-11-24 21:35:29 -0500

    remove signup button check

--- tests/Feature/InstanceTest.php
+++ tests/Feature/InstanceTest.php
@@ -11,24 +11,6 @@ class InstanceTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * Check if, by default, the disable signups feature is turned off in an
-     * instance.
-     *
-     * @return void
-     */
-    public function test_disable_signup_set_to_false_shows_signup_button()
-    {
-        config(['monica.disable_signup' => false]);
-        factory(Account::class)->create();
-
-        $response = $this->get('/');
-
-        $response->assertSee(
-            'Sign up'
-        );
-    }
-
-    /**
      * If an instance sets `disable_signup` env variable to true, it should hide
      * the signup button on the Sign in page.
      * Also, trying to reach `/register` should lead to a 403 page.
